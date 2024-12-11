65aaf7b93cc7add5d372cd4598173f0c3e917d78
cesaramehri <cesar.amehri@gmail.com>
2024-12-11 14:13:58 +1300

    Badge 1 - Data Warehousing Workshop: Meet DORA!

---
+++ Badge1-DataWarehousingWorkshop/#6. Meet_DORA/#6.3. DORA_DWWs1-5.sql
@@ -0,0 +1,54 @@
+-- Setup
+USE DATABASE UTIL_DB;
+USE SCHEMA PUBLIC;
+USE ROLE ACCOUNTADMIN;
+
+-- DWW01
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT
+ 'DWW01' as step
+ ,( select count(*)  
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name in ('FLOWERS','VEGGIES','FRUITS')) as actual
+  ,3 as expected
+  ,'Created 3 Garden Plant schemas' as description
+);
+
+-- DWW02
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW02' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA 
+   where schema_name = 'PUBLIC') as actual 
+ , 0 as expected 
+ ,'Deleted PUBLIC schema.' as description
+);
+
+-- DWW03
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW03' as step 
+ ,( select count(*) 
+   from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+   where table_name = 'ROOT_DEPTH') as actual 
+ , 1 as expected 
+ ,'ROOT_DEPTH Table Exists' as description
+);
+
+-- DWW04
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'DWW04' as step
+ ,( select count(*) as SCHEMAS_FOUND 
+   from UTIL_DB.INFORMATION_SCHEMA.SCHEMATA) as actual
+ , 2 as expected
+ , 'UTIL_DB Schemas' as description
+);
+
+-- DWW05
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from ( 
+ SELECT 'DWW05' as step 
+,( select row_count 
+  from GARDEN_PLANTS.INFORMATION_SCHEMA.TABLES 
+  where table_name = 'ROOT_DEPTH') as actual 
+, 3 as expected 
+,'ROOT_DEPTH row count' as description
+);
