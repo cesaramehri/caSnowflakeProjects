b93f6558d101fcd25e3093306cc3b191545a0aec
cesaramehri <cesar.amehri@gmail.com>
2024-12-15 16:52:06 +1300

    Badge 2: Join Local to Shared Data

---
+++ snowflakeLearn/Hands-OnBadges/Badge2-Collaboration_Marketplace_CostEstimationWorkshop/#3. JoinLocalwSharedData/#3.2. DORA_CMCWs1-7.sql
@@ -0,0 +1,76 @@
+-- Setup
+USE DATABASE UTIL_DB;
+USE SCHEMA PUBLIC;
+USE ROLE ACCOUNTADMIN;
+
+-- Checks
+select grader(step, (actual = expected), actual, expected, description) as graded_results from( 
+ SELECT 'CMCW01' as step
+ ,( select count(*) 
+   from snowflake.account_usage.databases
+   where database_name = 'INTL_DB' 
+   and deleted is null) as actual
+ , 1 as expected
+ ,'Created INTL_DB' as description
+ );
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from(
+SELECT 'CMCW02' as step
+ ,( select count(*) 
+   from INTL_DB.INFORMATION_SCHEMA.TABLES 
+   where table_schema = 'PUBLIC' 
+   and table_name = 'INT_STDS_ORG_3166') as actual
+ , 1 as expected
+ ,'ISO table created' as description
+);
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from( 
+SELECT 'CMCW03' as step 
+ ,(select row_count 
+   from INTL_DB.INFORMATION_SCHEMA.TABLES  
+   where table_name = 'INT_STDS_ORG_3166') as actual 
+ , 249 as expected 
+ ,'ISO Table Loaded' as description 
+);
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from(
+SELECT 'CMCW04' as step
+ ,( select count(*) 
+   from INTL_DB.PUBLIC.NATIONS_SAMPLE_PLUS_ISO) as actual
+ , 249 as expected
+ ,'Nations Sample Plus Iso' as description
+);
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from(
+SELECT 'CMCW05' as step
+ ,(select row_count 
+  from INTL_DB.INFORMATION_SCHEMA.TABLES 
+  where table_schema = 'PUBLIC' 
+  and table_name = 'COUNTRY_CODE_TO_CURRENCY_CODE') as actual
+ , 265 as expected
+ ,'CCTCC Table Loaded' as description
+);
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from(
+SELECT 'CMCW06' as step
+ ,(select row_count 
+  from INTL_DB.INFORMATION_SCHEMA.TABLES 
+  where table_schema = 'PUBLIC' 
+  and table_name = 'CURRENCIES') as actual
+ , 151 as expected
+ ,'Currencies table loaded' as description
+);
+
+--
+select grader(step, (actual = expected), actual, expected, description) as graded_results from(
+ SELECT 'CMCW07' as step 
+,( select count(*) 
+  from INTL_DB.PUBLIC.SIMPLE_CURRENCY ) as actual
+, 265 as expected
+,'Simple Currency Looks Good' as description
+);
