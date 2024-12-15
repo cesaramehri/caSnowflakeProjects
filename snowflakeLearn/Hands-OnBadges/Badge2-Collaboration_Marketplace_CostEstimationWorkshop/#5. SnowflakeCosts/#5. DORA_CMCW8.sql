5ac039f2550676d2916484cf779421e60b6a6eda
cesaramehri <cesar.amehri@gmail.com>
2024-12-16 08:59:30 +1300

    Badge 2: Sharing Data to Accounts, Costs and Marketplace

---
+++ snowflakeLearn/Hands-OnBadges/Badge2-Collaboration_Marketplace_CostEstimationWorkshop/#5. SnowflakeCosts/#5. DORA_CMCW8.sql
@@ -0,0 +1,19 @@
+-- Setup
+USE DATABASE UTIL_DB;
+USE SCHEMA PUBLIC;
+USE ROLE ACCOUNTADMIN;
+
+--This DORA Check Requires that you RUN two Statements, one right after the other
+show shares in account;
+
+--the above command puts information into memory that can be accessed using result_scan(last_query_id())
+-- If you have to run this check more than once, always run the SHOW command immediately prior
+select grader(step, (actual = expected), actual, expected, description) as graded_results from (
+ SELECT 'CMCW08' as step
+ ,( select IFF(count(*)>0,1,0) 
+    from table(result_scan(last_query_id())) 
+    where "kind" = 'OUTBOUND'
+    and "database_name" = 'INTL_DB') as actual
+ , 1 as expected
+ ,'Outbound Share Created From INTL_DB' as description
+); 
