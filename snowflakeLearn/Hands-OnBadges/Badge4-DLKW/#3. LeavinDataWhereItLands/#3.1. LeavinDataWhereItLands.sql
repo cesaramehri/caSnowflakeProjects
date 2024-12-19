8bec727b92e8d2473ba59f0a48436a3caf2cff52
cesaramehri <cesar.amehri@gmail.com>
2024-12-20 09:22:25 +1300

    Badge 4: Lesson 3 - Leaving the Data Where it Lands

---
+++ snowflakeLearn/Hands-OnBadges/Badge4-DLKW/#3. LeavinDataWhereItLands/#3.1. LeavinDataWhereItLands.sql
@@ -0,0 +1,44 @@
+use role accountadmin;
+
+select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123 ) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+);
+
+-- 
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+ 'DLKW01' as step
+  ,( select count(*)  
+      from ZENAS_ATHLEISURE_DB.INFORMATION_SCHEMA.STAGES 
+      where stage_schema = 'PRODUCTS'
+      and 
+      (stage_type = 'Internal Named' 
+      and stage_name = ('PRODUCT_METADATA'))
+      or stage_name = ('SWEATSUITS')
+   ) as actual
+, 2 as expected
+, 'Zena stages look good' as description
+);
+
+--
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+   'DLKW02' as step
+   ,( select sum(tally) from
+        ( select count(*) as tally
+        from ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATBAND_PRODUCT_LINE
+        where length(product_code) > 7 
+        union
+        select count(*) as tally
+        from ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATSUIT_SIZES
+        where LEFT(sizes_available,2) = char(13)||char(10))     
+     ) as actual
+   ,0 as expected
+   ,'Leave data where it lands.' as description
+); 
